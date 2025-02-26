92e027560b3820fc4f5ea3685aa48c47de8fea0c
Tiara <ht.nguyenthi@gmail.com>
2025-02-26 13:58:36 +0100

    implementing updating user streak feature

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -9,6 +10,7 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=100, blank=True, null=True)
     xp_points = models.IntegerField(default=0) # xp for gamification
     streak_days = models.IntegerField(default=0) # streak days for gamification
+    last_completed_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -27,3 +29,19 @@ class CustomUser(AbstractUser):
 
     groups = models.ManyToManyField('auth.Group', related_name='customuser_set', blank=True)
     user_permissions = models.ManyToManyField('auth.Permission', related_name='customuser_set', blank=True)
+
+    # Updating user's streak
+    def update_streak(self):
+        today = now().date()
+
+        if self.last_completed_date == today:
+            return
+
+        if self.last_completed_date == None:
+            self.streak_days = 1
+        elif (today - self.last_completed_date).days == 1:
+            self.streak_days += 1
+        else:
+            self.streak_days = 1
+
+        self.save()
